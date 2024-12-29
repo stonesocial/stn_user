bdbc24ae6f04484b4fa25b934718a47bcfeab117
stonesocial <stonesocial@outlook.com>
2024-12-29 03:05:39 +0200

    ref: performance improvements

--- lib/helpers/handlers.dart
+++ lib/helpers/handlers.dart
@@ -1,7 +1,6 @@
 import '../stn_user.dart';
 
 extension ListHandler on List<User> {
-  void get sortAsc => sort((a, b) => b.createdAt.compareTo(a.createdAt));
   List<User> keepCurrentAlive(List<User> currentState) => where((e) => !currentState.map((e) => e.pubKey).contains(e.pubKey)).toList();
 }
 extension Handler on User {
